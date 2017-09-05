6f2126a3cbf22e3826bef3c31ddcef3b3f4b5b1d
Daniel Supplieth <supplieth@modell-aachen.de>
2017-09-05 15:08:54 +0200

    Fix SVn ref in version string.
    id: none
    
    Ticket: none
    Release-Note: none

--- lib/Foswiki/Contrib/PatternSkin.pm
+++ lib/Foswiki/Contrib/PatternSkin.pm
@@ -3,7 +3,7 @@ package Foswiki::Contrib::PatternSkin;
 use strict;
 use warnings;
 
-our $VERSION = '$Rev$';
+our $VERSION = '5.4';
 our $RELEASE = "5.4";
 our $SHORTDESCRIPTION =
 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
